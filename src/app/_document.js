6e1fa576b52e04804a9f329639a83ee05527aed5
Duy Tan <donatemeplease15@gmail.com>
2024-11-13 17:05:14 +0700

    add Google Analytics g-tag

---
+++ src/app/_document.js
@@ -0,0 +1,37 @@
+// pages/_document.js  (Create this file if it doesn't exist)
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+      return (
+        <Html>
+          <Head>
+
+
+            {/* Google tag (gtag.js) */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                    `,
+                }}
+            />
+
+
+
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      );
+    }
+  }
