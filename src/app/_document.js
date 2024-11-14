83c9a91647d02d9bca9c08de7783944fbd01af47
Duy Tan <donatemeplease15@gmail.com>
2024-11-14 17:13:48 +0700

    use orignial google tag

--- src/app/_document.js
+++ src/app/_document.js
@@ -3,15 +3,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/head';
 
 export default class MyDocument extends Document {
     render() {
+      console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
       return (
         <Html>
           <Head>
-
-
             {/* Google tag (gtag.js) */}
             <script
               async
-              src="https://www.googletagmanager.com/gtag/js?id=G-2MY4QC3HK9"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
             />
             <script
                 dangerouslySetInnerHTML={{
@@ -19,13 +18,10 @@ export default class MyDocument extends Document {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', 'G-2MY4QC3HK9');
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                     `,
                 }}
             />
-
-
-
           </Head>
           <body>
             <Main />
